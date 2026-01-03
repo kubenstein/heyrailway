ff2f4046dfa3e1efea575bc562a6f58f22fda29d
Jakub Niewczas <niewczas.jakub@gmail.com>
2026-01-03 21:53:04 +0900

    implement game wrapper, action buttons, cta, footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,78 @@
 import { useState } from 'react';
 import GameApp from './gameApp';
-import './index.css';
-const styles = {};
+import styles from './App.module.css';
 
 export default function App() {
   const [showGame, setShowGame] = useState(false);
 
+  const startGame = () => {
+    setShowGame(true);
+    setTimeout(() => {
+      const scrollToEl = document.getElementById('gameScrollAnchor');
+      scrollToEl?.scrollIntoView({ behavior: 'smooth' });
+    }, 100);
+  };
+
   const scrollToDocumentation = () => {
-    const el = document.getElementById('documentation');
-    el?.scrollIntoView({ behavior: 'smooth' });
+    const scrollToEl = document.getElementById('documentationScrollAnchor');
+    scrollToEl?.scrollIntoView({ behavior: 'smooth' });
   };
 
   return (
-    <div>
-      <div className={styles.story}>
-        <h1>Hey Railway!</h1>
-        <p>
-          Welcome to Hey Railway!, a game where you build and manage a railway system to transport various types of
-          cargo between stations. Your goal is to optimize your railway network, upgrade your stations and carts, and
-          ensure timely deliveries while handling increasing demand.
-        </p>
-      </div>
-      <div className={styles.actions}>
-        <button onClick={() => setShowGame((v) => !v)}>Start Game</button>
-        <button onClick={scrollToDocumentation}>Scroll to Documentation</button>
-      </div>
-      {showGame ? (
-        <div className={styles.gameWrapper}>
-          <GameApp />
+    <main className={styles.host}>
+      <div className={styles.main}>
+        <div className={styles.story}>
+          <strong>Hey Railway!</strong>
+          <p>
+            Welcome to Hey Railway!, a game where you build and manage a railway system to transport various types of
+            cargo between stations. Your goal is to optimize your railway network, upgrade your stations and carts, and
+            ensure timely deliveries while handling increasing demand.
+          </p>
         </div>
-      ) : (
-        <div>game preview</div>
-      )}
 
-      <div className={styles.documentation} id="documentation">
-        <h2>Documentation</h2>
+        <div className={styles.actions}>
+          <button className={styles.game} onClick={startGame} disabled={showGame}>
+            Start Game
+          </button>
+          <button className={styles.docs} onClick={scrollToDocumentation}>
+            Scroll to Documentation
+          </button>
+        </div>
+
+        {showGame ? (
+          <div id="game" className={styles.gameWrapper}>
+            <div id="gameScrollAnchor" className={styles.gameScrollAnchor} />
+            <GameApp />
+          </div>
+        ) : (
+          <div>game preview</div>
+        )}
+
+        <div id="documentationScrollAnchor" className={styles.documentation}>
+          <strong>Technical Documentation</strong>
+          <div style={{ height: '30rem' }} />
+        </div>
+
+        <div className={styles.cta}>
+          <strong>Would you like to have a quick chat?</strong>
+          <a target="_blank" href="https://www.jakubniewczas.pl/" rel="noreferrer">
+            https://jakubniewczas.pl
+          </a>
+          <div className={styles.board}>
+            <i className={styles.me} />
+            <i className={styles.railway} />
+          </div>
+        </div>
+
+        <div className={styles.footer}>
+          Made with ♥ by{' '}
+          <a target="_blank" href="https://jakubniewczas.pl" rel="noreferrer">
+            Jakub Niewczas
+          </a>
+          <br />
+          during the 2025/2026 New Year&apos;s holidays
+        </div>
       </div>
-      <div className={styles.footer}>
-        Made with ♥ during 2025/2026 new year holiday by <a href="https://jakubniewczas.pl">Jakub Niewczas</a>
-      </div>
-    </div>
+    </main>
   );
 }
